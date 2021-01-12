a085a324f4780b2f6a113d0fe7759f07bbb93944
yilmaz <yilmaz@auburn.edu>
2021-01-12 17:44:54 -0600

    iac package is imported

--- src/main/scala/SocialSimMain.scala
+++ src/main/scala/SocialSimMain.scala
@@ -7,6 +7,8 @@ import breeze.linalg.operators.BitVectorOps
 
 import scalaSim._
 import scalAgent._
+import  iac._
+import iac.Declarations._
 
 
 object SocialSimMain extends App {
@@ -35,6 +37,32 @@ object SocialSimMain extends App {
       }
     }
   }.simulate()
+
+  println("------------------------")
+
+  var n = Narrative {
+    evidence("e", 0.2)
+    evidence("e1", 1.0)
+    belief("a", 0.5)
+    belief("b", 0.2)
+    belief("c", 0.4)
+  } subjectTo {
+    explain("b", "e", 0.5)
+    explain(List("a", "c"), "e1", 0.5)
+    contradict("a", "b", 1)
+  }
+
+  println(n.activations)
+  println(n.constraintMatrix)
+
+  n.simulate()
+  println(n.activations)
+
+  println("---------------------")
+
+  IACDSLTest()
+
+  IACUtilities()
 
   //loadModule("/Users/yilmaz/IdeaProjects/SocialSim/src/FCM.py",interp)
 
